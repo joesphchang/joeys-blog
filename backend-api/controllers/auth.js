e879d1bf566c509dd3d101b7d05de31df8388a8e
joesphchang <joesphschang@gmail.com>
2023-03-14 19:52:43 -0500

    fixed bugs for user data

--- backend-api/controllers/auth.js
+++ backend-api/controllers/auth.js
@@ -3,35 +3,37 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 export const register = (req, res) => {
-	// CHECK EXISITING USERS
+	//CHECK EXISTING USER
 	const q = 'SELECT * FROM users WHERE email = ? OR username = ?';
 
-	db.query(q, [req.body.email, req.body.name], (err, data) => {
-		if (err) return res.json(err);
+	db.query(q, [req.body.email, req.body.username], (err, data) => {
+		if (err) return res.status(500).json(err);
 		if (data.length) return res.status(409).json('User already exists!');
 
-		// Hash the passworsd and create a user
+		//Hash the password and create a user
 		const salt = bcrypt.genSaltSync(10);
 		const hash = bcrypt.hashSync(req.body.password, salt);
 
-		const q = 'INSERT INTO users(`username`, `email`, `password`) VALUES (?)';
+		const q = 'INSERT INTO users(`username`,`email`,`password`) VALUES (?)';
 		const values = [req.body.username, req.body.email, hash];
 
 		db.query(q, [values], (err, data) => {
-			if (err) return res.json(err);
-			return res.status(200).json('User has been created!');
+			if (err) return res.status(500).json(err);
+			return res.status(200).json('User has been created.');
 		});
 	});
 };
 
 export const login = (req, res) => {
-	// CHECK USER
-	const q = 'SELECT * FROM users WHERE EMAIL = ? OR username =?';
+	//CHECK USER
+
+	const q = 'SELECT * FROM users WHERE username = ?';
 
 	db.query(q, [req.body.username], (err, data) => {
-		if (err) return res.json(err);
+		if (err) return res.status(500).json(err);
 		if (data.length === 0) return res.status(404).json('User not found!');
-		// Check password
+
+		//Check password
 		const isPasswordCorrect = bcrypt.compareSync(
 			req.body.password,
 			data[0].password
@@ -52,4 +54,9 @@ export const login = (req, res) => {
 	});
 };
 
-export const logout = (req, res) => {};
+export const logout = (req, res) => {
+	res.clearCookie("access_token", {
+		sameSite: "none",
+		secure: true
+	}).status(200).json('User has been logged out!');
+};
--- backend-api/index.js
+++ backend-api/index.js
@@ -3,15 +3,18 @@ import authRoutes from './routes/auth.js';
 import userRoutes from './routes/users.js'
 import postRoutes from './routes/posts.js';
 import cookieParser from "cookie-parser";
+import cors from 'cors';
 
 const app = express()
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use('/api/posts', postRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 
-app.listen(8800, () => {
+app.listen(8000, () => {
     console.log('Connected!');
 })
